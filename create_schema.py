000dff5d43f09fcc018b26270e24285bfa3bfad5
boonkwee <chan_boonkwee@yahoo.com>
2024-10-10 21:52:04 +0800

    Updated create_schema.py to create settings.json if file does not exist.

--- create_schema.py
+++ create_schema.py
@@ -1,6 +1,9 @@
 from sqlalchemy import inspect
 from database import engine, Base
 from models import Location, PostalCode, OneMapResponse
+import os
+import json
+from settings import json_file
 
 inspector = inspect(engine)
 default_schema = inspector.get_schema_names()[0]
@@ -9,4 +12,9 @@ location_exist = Location.__tablename__ in tbl_list
 postalcode_exist = PostalCode.__tablename__ in tbl_list
 onemapr_exist = OneMapResponse.__tablename__ in tbl_list
 if not (location_exist and postalcode_exist and onemapr_exist):
-    Base.metadata.create_all(bind=engine)
+  Base.metadata.create_all(bind=engine)
+
+if not os.path.exists(json_file):
+  with open(json_file, 'w') as fp:
+    json.dump([1, 10000], fp)
+    fp.close()
